27f6961ece82ab53355b08b9d8b54922d657ee6c
arunkindo2000-eng <arunkindo2000@gmail.com>
2026-02-07 14:40:52 +0530

    Implement user pairing in WebSocket server
    Refactor WebSocket server to handle user pairing and messaging.

--- server.js
+++ server.js
@@ -7,14 +7,44 @@ let clients = [];
 wss.on("connection", function(ws) {
   clients.push(ws);
 
-  ws.on("message", function(message) {
-    // send message to any other connected user
-    clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
+  const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 10000 });
+
+let waitingUser = null;
+
+wss.on("connection", ws => {
+
+  if (waitingUser === null) {
+    waitingUser = ws;
+    ws.send("Waiting for a stranger...");
+  } else {
+    ws.partner = waitingUser;
+    waitingUser.partner = ws;
+
+    ws.send("Stranger connected!");
+    waitingUser.send("Stranger connected!");
+
+    waitingUser = null;
+  }
+
+  ws.on("message", msg => {
+    if (ws.partner && ws.partner.readyState === WebSocket.OPEN) {
+      ws.partner.send(msg.toString());
+    }
   });
+
+  ws.on("close", () => {
+    if (ws.partner) {
+      ws.partner.send("Stranger disconnected.");
+      ws.partner.partner = null;
+    }
+    if (waitingUser === ws) {
+      waitingUser = null;
+    }
+  });
+});
+
+console.log("WebSocket server running");
 
   ws.on("close", function() {
     clients = clients.filter(client => client !== ws);
